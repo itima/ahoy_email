0e7ee5aa805b46150651377661ce05988893ab1f
Alexey Vorobiev <vorobiev.alexey@gmail.com>
2014-06-30 15:23:26 +0400

    fixed engine send email

--- lib/ahoy_email/engine.rb
+++ lib/ahoy_email/engine.rb
@@ -1,11 +1,12 @@
 module AhoyEmail
   class Engine < ::Rails::Engine
+    engine_name 'ahoy_email_engine'
 
     initializer "ahoy_email" do |app|
       AhoyEmail.secret_token = app.config.respond_to?(:secret_key_base) ? app.config.secret_key_base : app.config.secret_token
       if AhoyEmail.secret_token.blank?
-		    AhoyEmail.secret_token = Rails.application.secrets.secret_key_base
-	    end
+      	AhoyEmail.secret_token = Rails.application.secrets.secret_key_base
+      end
     end
 
   end
